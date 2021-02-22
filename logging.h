3447a3d96bf3f3548da66da385a7578b9e763bf9
Peter Bieringer <pb@bieringer.de>
2021-02-22 22:00:40 +0100

    add additional logging

--- logging.h
+++ logging.h
@@ -8,13 +8,14 @@ extern int m_debugmask;
 
 #define DEBUG_MASK_RB_OSD	0x00010000	// OSD general
 #define DEBUG_MASK_RB_OSD_DT	0x00020000	// OSD draw text
+#define DEBUG_MASK_RB_PICT	0x00100000	// fs453settings
 
 #if 1
 
 #define isyslog_rb(format, arg...) isyslog("reelbox: INFO  " format, ## arg)
 #define esyslog_rb(format, arg...) esyslog("reelbox: ERROR " format, ## arg)
-#define dsyslog_rb(format, arg...) dsyslog("reelbox: DEBUG %s/%s " format, __FILE__, __FUNCTION__, ## arg)
-#define printf(format, arg...)     dsyslog("reelbox: DEBUG %s/%s " format, __FILE__, __FUNCTION__, ## arg)
+#define dsyslog_rb(format, arg...) dsyslog("reelbox: DEBUG %s/%s: " format, __FILE__, __FUNCTION__, ## arg)
+#define printf(format, arg...)     dsyslog("reelbox: DEBUG %s/%s: " format, __FILE__, __FUNCTION__, ## arg)
 
 #else
 
@@ -28,5 +29,6 @@ extern int m_debugmask;
 
 #define DEBUG_RB_OSD	if (m_debugmask & DEBUG_MASK_RB_OSD)    dsyslog_rb
 #define DEBUG_RB_OSD_DT	if (m_debugmask & DEBUG_MASK_RB_OSD_DT) dsyslog_rb
+#define DEBUG_RB_PICT	if (m_debugmask & DEBUG_MASK_RB_PICT)   dsyslog_rb
 
 #endif
