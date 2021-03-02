6d2da8baf69b9482e6bdabbbe3e0e2b6ea80243c
Peter Bieringer <pb@bieringer.de>
2021-03-02 21:19:53 +0100

    add DrawPixmap function

--- HdFbTrueColorOsd.h
+++ HdFbTrueColorOsd.h
@@ -141,6 +141,8 @@ static unsigned char *mySavedRegion = NULL;
 
                        void UpdateDirty(int x0, int y0, int x1, int y1);
 
+		       void DrawPixmap(const int X, const int Y, const uint8_t *pmData, const int w, const int h, const int s);
+
 	// virtual cPixmap *CreatePixmap(int Layer, const cRect &ViewPort, const cRect &DrawPort = cRect::Null);
         
     private:
