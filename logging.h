edb048ce078d5687fdd71c4a7678194e2f1178d0
Peter Bieringer <pb@bieringer.de>
2021-03-03 09:09:13 +0100

    add pixmap debug log support

--- logging.h
+++ logging.h
@@ -10,7 +10,8 @@ extern int m_debugmask;
 #define DEBUG_MASK_RB_OSD_DT	0x00020000	// OSD draw text
 #define DEBUG_MASK_RB_OSD_SP	0x00040000	// OSD special
 #define DEBUG_MASK_RB_OSD_AR	0x00080000	// OSD area related
-#define DEBUG_MASK_RB_OSD_BM	0x00100000	// OSD area bitmap related
+#define DEBUG_MASK_RB_OSD_BM	0x00100000	// OSD Bitmap related
+#define DEBUG_MASK_RB_OSD_PM	0x00000100	// OSD Pixmap related
 #define DEBUG_MASK_RB_OSD_AC	0x01000000	// OSD active
 #define DEBUG_MASK_RB_PICT	0x10000000	// fs453settings
 #define DEBUG_MASK_VPHD		0x00001000	// VideoPlayerHd
@@ -28,8 +29,8 @@ extern int m_debugmask;
 // stderr debugging
 #define isyslog_rb(format, arg...) fprintf(stderr, "reelbox: INFO  " format, ## arg)
 #define esyslog_rb(format, arg...) fprintf(stderr, "reelbox: ERROR " format, ## arg)
-#define dsyslog_rb(format, arg...) fprintf(stderr, "reelbox: DEBUG " format, ## arg)
-#define printf(format, arg...)     fprintf(stderr, "reelbox: DEBUG " format, ## arg)
+#define dsyslog_rb(format, arg...) fprintf(stderr, "reelbox: DEBUG %s/%s " format, __FILE__, __FUNCTION__, ## arg)
+#define printf(format, arg...)     fprintf(stderr, "reelbox: DEBUG %s/%s " format, __FILE__, __FUNCTION__, ## arg)
 
 #endif
 
@@ -38,6 +39,7 @@ extern int m_debugmask;
 #define DEBUG_RB_OSD_SP	if (m_debugmask & DEBUG_MASK_RB_OSD_SP) dsyslog_rb
 #define DEBUG_RB_OSD_AR	if (m_debugmask & DEBUG_MASK_RB_OSD_AR) dsyslog_rb
 #define DEBUG_RB_OSD_BM	if (m_debugmask & DEBUG_MASK_RB_OSD_BM) dsyslog_rb
+#define DEBUG_RB_OSD_PM	if (m_debugmask & DEBUG_MASK_RB_OSD_PM) dsyslog_rb
 #define DEBUG_RB_OSD_AC	if (m_debugmask & DEBUG_MASK_RB_OSD_AC) dsyslog_rb
 #define DEBUG_RB_PICT	if (m_debugmask & DEBUG_MASK_RB_PICT)   dsyslog_rb
 #define DEBUG_RB_VPHD	if (m_debugmask & DEBUG_MASK_VPHD)	dsyslog_rb
