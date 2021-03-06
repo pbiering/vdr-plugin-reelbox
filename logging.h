9eca1a737d963892677c9d179f7b871ae00a30c1
Peter Bieringer <pb@bieringer.de>
2021-03-06 16:31:49 +0100

    add/adjust logging mask

--- logging.h
+++ logging.h
@@ -11,9 +11,10 @@ extern int m_debugmask;
 #define DEBUG_MASK_RB_OSD_SP	0x00040000	// OSD special
 #define DEBUG_MASK_RB_OSD_AR	0x00080000	// OSD area related
 #define DEBUG_MASK_RB_OSD_BM	0x00100000	// OSD Bitmap related
-#define DEBUG_MASK_RB_OSD_PM	0x00000100	// OSD Pixmap related
-#define DEBUG_MASK_RB_OSD_FL	0x00000200	// OSD Flush related
+#define DEBUG_MASK_RB_OSD_PM	0x00200000	// OSD Pixmap related
+#define DEBUG_MASK_RB_OSD_FL	0x00800000	// OSD Flush related
 #define DEBUG_MASK_RB_OSD_AC	0x01000000	// OSD active
+#define DEBUG_MASK_RB_OSD_CO	0x02000000	// OSD create
 #define DEBUG_MASK_RB_PICT	0x10000000	// fs453settings
 #define DEBUG_MASK_VPHD		0x00001000	// VideoPlayerHd
 #define DEBUG_MASK_PLAYTS	0x00000010	// PlayTS
@@ -43,6 +44,7 @@ extern int m_debugmask;
 #define DEBUG_RB_OSD_PM	if (m_debugmask & DEBUG_MASK_RB_OSD_PM) dsyslog_rb
 #define DEBUG_RB_OSD_FL	if (m_debugmask & DEBUG_MASK_RB_OSD_FL) dsyslog_rb
 #define DEBUG_RB_OSD_AC	if (m_debugmask & DEBUG_MASK_RB_OSD_AC) dsyslog_rb
+#define DEBUG_RB_OSD_CO	if (m_debugmask & DEBUG_MASK_RB_OSD_CO) dsyslog_rb
 #define DEBUG_RB_PICT	if (m_debugmask & DEBUG_MASK_RB_PICT)   dsyslog_rb
 #define DEBUG_RB_VPHD	if (m_debugmask & DEBUG_MASK_VPHD)	dsyslog_rb
 #define DEBUG_RB_PLAYTS	if (m_debugmask & DEBUG_MASK_PLAYTS)	dsyslog_rb
