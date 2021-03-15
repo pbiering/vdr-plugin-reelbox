6cc4809f56c9da37cd4aadb7e515f2f6482704d9
Peter Bieringer <pb@bieringer.de>
2021-03-15 09:01:52 +0100

    add/shift debugmask entries

--- logging.h
+++ logging.h
@@ -7,7 +7,9 @@
 extern int m_debugmask;
 
 #define DEBUG_MASK_RB_OSD	0x00010000	// OSD general
-#define DEBUG_MASK_RB_OSD_DT	0x00020000	// OSD draw text
+#define DEBUG_MASK_RB_OSD_DT	0x00008000	// OSD draw text
+#define DEBUG_MASK_RB_OSD_DF	0x00001000	// OSD draw figures
+#define DEBUG_MASK_RB_OSD_UD	0x00002000	// OSD update dirty
 #define DEBUG_MASK_RB_OSD_SP	0x00040000	// OSD special
 #define DEBUG_MASK_RB_OSD_AR	0x00080000	// OSD area related
 #define DEBUG_MASK_RB_OSD_BM	0x00100000	// OSD Bitmap related
@@ -16,21 +18,21 @@ extern int m_debugmask;
 #define DEBUG_MASK_RB_OSD_AC	0x01000000	// OSD active
 #define DEBUG_MASK_RB_OSD_CO	0x02000000	// OSD create
 #define DEBUG_MASK_RB_PICT	0x10000000	// fs453settings
-#define DEBUG_MASK_VPHD		0x00001000	// VideoPlayerHd
+#define DEBUG_MASK_VPHD		0x00000100	// VideoPlayerHd
 #define DEBUG_MASK_PLAYTS	0x00000010	// PlayTS
 
-#if 1
+#if 0
 
-#define isyslog_rb(format, arg...) isyslog("reelbox: INFO  " format, ## arg)
-#define esyslog_rb(format, arg...) esyslog("reelbox: ERROR " format, ## arg)
+#define isyslog_rb(format, arg...) isyslog("reelbox: INFO  %s " format, __FUNCTION__, ## arg)
+#define esyslog_rb(format, arg...) esyslog("reelbox: ERROR %s " format, __FUNCTION__, ## arg)
 #define dsyslog_rb(format, arg...) dsyslog("reelbox: DEBUG %s/%s: " format, __FILE__, __FUNCTION__, ## arg)
 #define printf(format, arg...)     dsyslog("reelbox: DEBUG %s/%s: " format, __FILE__, __FUNCTION__, ## arg)
 
 #else
 
 // stderr debugging
-#define isyslog_rb(format, arg...) fprintf(stderr, "reelbox: INFO  " format, ## arg)
-#define esyslog_rb(format, arg...) fprintf(stderr, "reelbox: ERROR " format, ## arg)
+#define isyslog_rb(format, arg...) fprintf(stderr, "reelbox: INFO  %s/%s " format, __FILE__, __FUNCTION__, ## arg)
+#define esyslog_rb(format, arg...) fprintf(stderr, "reelbox: ERROR %s/%s " format, __FILE__, __FUNCTION__, ## arg)
 #define dsyslog_rb(format, arg...) fprintf(stderr, "reelbox: DEBUG %s/%s " format, __FILE__, __FUNCTION__, ## arg)
 #define printf(format, arg...)     fprintf(stderr, "reelbox: DEBUG %s/%s " format, __FILE__, __FUNCTION__, ## arg)
 
@@ -38,6 +40,8 @@ extern int m_debugmask;
 
 #define DEBUG_RB_OSD	if (m_debugmask & DEBUG_MASK_RB_OSD)    dsyslog_rb
 #define DEBUG_RB_OSD_DT	if (m_debugmask & DEBUG_MASK_RB_OSD_DT) dsyslog_rb
+#define DEBUG_RB_OSD_DF	if (m_debugmask & DEBUG_MASK_RB_OSD_DF) dsyslog_rb
+#define DEBUG_RB_OSD_UD	if (m_debugmask & DEBUG_MASK_RB_OSD_UD) dsyslog_rb
 #define DEBUG_RB_OSD_SP	if (m_debugmask & DEBUG_MASK_RB_OSD_SP) dsyslog_rb
 #define DEBUG_RB_OSD_AR	if (m_debugmask & DEBUG_MASK_RB_OSD_AR) dsyslog_rb
 #define DEBUG_RB_OSD_BM	if (m_debugmask & DEBUG_MASK_RB_OSD_BM) dsyslog_rb
